56bd5f26d01bbb78a2a69e587cdd4418f1fa310a
LauGR <laurent.gregoire974@gmail.com>
2018-02-08 16:55:00 +0400

    ok

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.dropdown-submenu a.test').on("click", function(e){
+  $('.dropdown-submenu a.linkdrop').on("click", function(e){
     $(this).next('ul').toggle();
     e.stopPropagation();
     e.preventDefault();
